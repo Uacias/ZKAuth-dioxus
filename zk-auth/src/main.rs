57112654982e52f3778293ceafec917d5b5f87a0
Uacias <uaciasdev@gmail.com>
2024-07-02 02:48:40 +0200

    basic dioxus

--- zk-auth/src/main.rs
+++ zk-auth/src/main.rs
@@ -1,40 +1,190 @@
-#![allow(non_snake_case)]
-
+use chrono::{DateTime, Utc};
 use dioxus::prelude::*;
-use tracing::{info, Level};
+use serde::{Deserialize, Serialize};
 
 fn main() {
-    // Init logger
-    dioxus_logger::init(Level::INFO).expect("failed to init logger");
     launch(App);
 }
 
-fn App() -> Element {
-    // Build cool things ✌️
-
+pub fn App() -> Element {
+    use_context_provider(|| Signal::new(PreviewState::Unset));
     rsx! {
-        link { rel: "stylesheet", href: "main.css" }
-        img { src: "header.svg", id: "header" }
-        div { id: "links",
-            a { href: "https://dioxuslabs.com/learn/0.5/", "📚 Learn Dioxus" }
-            a { href: "https://dioxuslabs.com/awesome", "🚀 Awesome Dioxus" }
-            a { href: "https://github.com/dioxus-community/", "📡 Community Libraries" }
-            a { href: "https://github.com/DioxusLabs/dioxus-std", "⚙️ Dioxus Standard Library" }
-            a { href: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus",
-                "💫 VSCode Extension"
-            }
-            a { href: "https://discord.gg/XgGxMSkvUM", "👋 Community Discord" }
+        div { display: "flex", flex_direction: "row", width: "100%",
+            div { width: "50%", Stories {} }
+            div { width: "50%", Preview {} }
         }
     }
 }
 
-#[server(PostServerData)]
-async fn post_server_data(data: String) -> Result<(), ServerFnError> {
-    info!("Server received: {}", data);
-    Ok(())
+// New
+fn Stories() -> Element {
+    rsx! {
+        StoryListing {
+            story: StoryItem {
+                id: 0,
+                title: "hello hackernews".to_string(),
+                url: None,
+                text: None,
+                by: "Author".to_string(),
+                score: 0,
+                descendants: 0,
+                time: chrono::Utc::now(),
+                kids: vec![],
+                r#type: "".to_string(),
+            }
+        }
+    }
 }
 
-#[server(GetServerData)]
-async fn get_server_data() -> Result<String, ServerFnError> {
-    Ok("Hello from the server!".to_string())
+// New
+#[derive(Clone, Debug)]
+enum PreviewState {
+    Unset,
+    Loading,
+    Loaded(StoryPageData),
+}
+
+// New
+fn Preview() -> Element {
+    let preview_state = consume_context::<Signal<PreviewState>>();
+    match preview_state() {
+        PreviewState::Unset => rsx! {"Hover over a story to preview it here"},
+        PreviewState::Loading => rsx! {"Loading..."},
+        PreviewState::Loaded(story) => {
+            rsx! {
+                div { padding: "0.5rem",
+                    div { font_size: "1.5rem", a { href: story.item.url, "{story.item.title}" } }
+                    div { dangerous_inner_html: story.item.text }
+                    for comment in &story.comments {
+                        Comment { comment: comment.clone() }
+                    }
+                }
+            }
+        }
+    }
+}
+
+// NEW
+#[component]
+fn Comment(comment: Comment) -> Element {
+    rsx! {
+        div { padding: "0.5rem",
+            div { color: "gray", "by {comment.by}" }
+            div { dangerous_inner_html: "{comment.text}" }
+            for kid in &comment.sub_comments {
+                Comment { comment: kid.clone() }
+            }
+        }
+    }
+}
+
+// Define the Hackernews types
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+pub struct StoryPageData {
+    #[serde(flatten)]
+    pub item: StoryItem,
+    #[serde(default)]
+    pub comments: Vec<Comment>,
+}
+
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+pub struct Comment {
+    pub id: i64,
+    /// there will be no by field if the comment was deleted
+    #[serde(default)]
+    pub by: String,
+    #[serde(default)]
+    pub text: String,
+    #[serde(with = "chrono::serde::ts_seconds")]
+    pub time: DateTime<Utc>,
+    #[serde(default)]
+    pub kids: Vec<i64>,
+    #[serde(default)]
+    pub sub_comments: Vec<Comment>,
+    pub r#type: String,
+}
+
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+pub struct StoryItem {
+    pub id: i64,
+    pub title: String,
+    pub url: Option<String>,
+    pub text: Option<String>,
+    #[serde(default)]
+    pub by: String,
+    #[serde(default)]
+    pub score: i64,
+    #[serde(default)]
+    pub descendants: i64,
+    #[serde(with = "chrono::serde::ts_seconds")]
+    pub time: DateTime<Utc>,
+    #[serde(default)]
+    pub kids: Vec<i64>,
+    pub r#type: String,
+}
+
+#[component]
+fn StoryListing(story: ReadOnlySignal<StoryItem>) -> Element {
+    let mut preview_state = consume_context::<Signal<PreviewState>>();
+    let StoryItem {
+        title,
+        url,
+        by,
+        score,
+        time,
+        kids,
+        ..
+    } = &*story.read();
+
+    let url = url.as_deref().unwrap_or_default();
+    let hostname = url
+        .trim_start_matches("https://")
+        .trim_start_matches("http://")
+        .trim_start_matches("www.");
+    let score = format!("{score} {}", if *score == 1 { " point" } else { " points" });
+    let comments = format!(
+        "{} {}",
+        kids.len(),
+        if kids.len() == 1 {
+            " comment"
+        } else {
+            " comments"
+        }
+    );
+    let time = time.format("%D %l:%M %p");
+
+    rsx! {
+        div { padding: "0.5rem", position: "relative",
+        onmouseenter: move |_event| {
+                *preview_state.write() =
+                    PreviewState::Loaded(StoryPageData {
+                        item: story(),
+                        comments: vec![],
+                    });
+            },
+            div { font_size: "1.5rem",
+                a { href: url,
+                    onfocus: move |_event| {
+                        *preview_state.write() =
+                            PreviewState::Loaded(StoryPageData {
+                                item: story(),
+                                comments: vec![],
+                            });
+                    }
+                    ,"{title}" }
+                a {
+                    color: "gray",
+                    href: "https://news.ycombinator.com/from?site={hostname}",
+                    text_decoration: "none",
+                    " ({hostname})"
+                }
+            }
+            div { display: "flex", flex_direction: "row", color: "gray",
+                div { color: "green", "{score}" }
+                div { padding_left: "0.5rem", "by {by}" }
+                div { padding_left: "0.5rem", "{time}" }
+                div { padding_left: "0.5rem", "{comments}" }
+            }
+        }
+    }
 }
